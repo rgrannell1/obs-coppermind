1fb3acdb21cd1edd5212f2d0c5cb0800cb023da7
Róisín Grannell <r.grannell2@gmail.com>
2022-01-23 20:35:48 +0000

    added additional test

--- pkg/db_test.go
+++ pkg/db_test.go
@@ -22,3 +22,39 @@ func TestCreateTables(test *testing.T) {
 
 	// check tables exist
 }
+
+func TestDropBookmarks(test *testing.T) {
+	db, err := NewCoppermindDb(":memory:")
+	if err != nil {
+		test.Errorf("error creating coppermind db: %v", err)
+	}
+
+	// create tables first
+	err = db.CreateTables()
+	if err != nil {
+		test.Errorf("error creating tables: %v", err)
+	}
+
+	// drop
+
+	err = db.DropBookmarks()
+	if err != nil {
+		test.Errorf("error dropping bookmarks: %v", err)
+	}
+}
+
+func TestInsertBookmark(test *testing.T) {
+
+}
+
+func TestUpdateLastUpdated(test *testing.T) {
+
+}
+
+func TestGetLastUpdated(test *testing.T) {
+
+}
+
+func TestPinboardChanged(test *testing.T) {
+
+}
